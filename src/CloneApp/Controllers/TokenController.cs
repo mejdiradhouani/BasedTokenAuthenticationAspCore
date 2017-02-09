c4d71fbb7cb7aed6c74d859fbb45aaf873ee46f6
MejdiRadhouai <MejdiRadhouai@MEJDI>
2017-02-09 01:25:11 +0100

    first commit

---
+++ src/CloneApp/Controllers/TokenController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CloneApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        private readonly TokenAuthOptions tokenOption;
+        
+        public TokenController(TokenAuthOptions t)
+        {
+            this.tokenOption = t;
+        }
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+      [HttpPost("login")]
+        public dynamic Post([FromBody]AuthReq req)
+        { var auth = false;
+            if(req.username=="test" &&req.password=="test")
+            {
+                DateTime? expire = DateTime.UtcNow.AddMinutes(2);
+                var token = GetToken(req.username,expire);
+                return new { auth = true, entityId = 1, token = token, tokenExpires = expire };
+
+
+            }
+            return new { auth = false };
+        }
+
+        private string GetToken(string username, DateTime? expire)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(username, "TokenAuth"), new[] { new Claim("EntityID", "1", ClaimValueTypes.Integer) });
+
+            var securityToken = handler.CreateToken(new Microsoft.IdentityModel.Tokens.SecurityTokenDescriptor()
+            {
+                Issuer = this.tokenOption.Issuer,
+                Audience = tokenOption.Audience,
+                SigningCredentials = tokenOption.SigningCredentials,
+                Subject = identity,
+                Expires = expire
+            });
+            return handler.WriteToken(securityToken);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/CloneApp/Startup.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CloneApp
+{
+    public class TokenAuthOptions
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+    public class Startup
+    {
+        RsaSecurityKey  key;
+        public TokenAuthOptions _tokenAuth;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+       
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(2048);
+            //Save the public key information to an RSAParameters structure.  
+            
+            RSAParameters KeyParam = RSA.ExportParameters(true);
+            key = new RsaSecurityKey(KeyParam);
+            _tokenAuth = new TokenAuthOptions
+            {
+                Audience = "TokenAudience",
+                Issuer= "Issuer",
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256Signature)
+            };
+
+            services.AddSingleton<TokenAuthOptions>(_tokenAuth);
+
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
+                    .RequireAuthenticatedUser().Build());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            _tokenAuth = new TokenAuthOptions
+            {
+                Audience = "TokenAudience",
+                Issuer = "Issuer",
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256Signature)
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = key,
+                    ValidAudience = _tokenAuth.Audience,
+                    ValidIssuer = _tokenAuth.Issuer,
+
+                    // When receiving a token, check that it is still valid.
+                    ValidateLifetime = true,
+
+                    // This defines the maximum allowable clock skew - i.e.
+                    // provides a tolerance on the token expiry time 
+                    // when validating the lifetime. As we're creating the tokens 
+                    // locally and validating them on the same machines which 
+                    // should have synchronised time, this can be set to zero. 
+                    // Where external tokens are used, some leeway here could be 
+                    // useful.
+                    ClockSkew = TimeSpan.FromMinutes(0)
+                }
+                
+
+            });
+            
+            app.UseMvc();
+        }
+    }
+}
